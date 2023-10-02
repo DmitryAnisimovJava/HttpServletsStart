a1c133ad6131bdb0ab52f20143be4688d2f2682e
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2023-10-02 17:13:51 +0300

    Add firstServlet.java

---
+++ src/main/java/com/dmdev/servlet/FirstServlet.java
@@ -0,0 +1,33 @@
+package com.dmdev.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		try (PrintWriter writer = resp.getWriter()) {
+			writer.write("Hello from First Servlet");
+		}
+	}
+
+	@Override
+	public void destroy() {
+		super.destroy();
+	}
+}
